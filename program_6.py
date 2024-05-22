dff7c3574c89caa009be8663852354f369eddaae
ReeceTeylan <rixteylan24@gmail.com>
2024-05-22 19:50:26 +0800

    Create class and import

---
+++ program_6.py
@@ -0,0 +1,4 @@
+# Create a class
+class TV:
+# Add constructor and instance variable
+# Add instance methods
---
+++ program_6_call.py
@@ -0,0 +1,3 @@
+from program_6 import TV
+# Create object of a class
+# Add call methods
