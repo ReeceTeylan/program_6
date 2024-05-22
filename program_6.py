a083ff2c167665bdd8874a1b9d7f7d3968f1e710
ReeceTeylan <rixteylan24@gmail.com>
2024-05-22 20:00:52 +0800

    Add conditions

--- program_6.py
+++ program_6.py
@@ -6,4 +6,22 @@ class TV:
         self.channel = channel
         self.volumeLevel = volume_level
         self.on = on
+
+        # Add conditions
+        if self.channel < 1:
+            self.channel = 1
+            print(f"{self.tvName}: There are no channels lower than 1")
+
+        if self.channel > 120:
+            self.channel = 120
+            print(f"{self.tvName}: There are no channels higher than 120")
+
+        if self.volumeLevel > 7:
+            self.volumeLevel = 7
+            print(f"{self.tvName}: 7 is the highest volume")
+
+        if self.volumeLevel < 1:
+            self.volumeLevel = 1
+            print(f"{self.tvName}: 1 is the lowest volume")
+
 # Add instance methods
