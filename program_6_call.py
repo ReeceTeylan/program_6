6c66bdc3075adb27202f8f7ea455ee3ad2bbde6a
ReeceTeylan <rixteylan24@gmail.com>
2024-05-22 21:41:52 +0800

    Add call methods

--- program_6_call.py
+++ program_6_call.py
@@ -4,3 +4,28 @@ from program_6 import TV
 tv1 = TV('tv1', 120, 2, True)
 tv2 = TV('tv2', 3, 5, False)
 # Add call methods
+
+#tv1
+tv1.turnOn()
+tv1.channel1up()
+tv1.setChannel(31)
+tv1.channel1down()
+tv1.setVolume(1)
+tv1.volumeDown
+tv1.volumeUp()
+tv1.volumeUp()
+tv1.turnOff
+tv1.turnOn
+
+#tv2
+tv2.turnOn()
+tv2.volumeUp()
+tv2.setChannel(3)
+tv2.volumeDown()
+tv2.volumeDown()
+tv2.volumeDown()
+tv2.volumeDown()
+
+#show
+tv1.show()
+tv2.show()
