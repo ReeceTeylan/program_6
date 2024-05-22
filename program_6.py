7bd69e9a2285155d4a000ad7f5bca20e5138c7f3
ReeceTeylan <rixteylan24@gmail.com>
2024-05-22 19:53:53 +0800

    Add constructor and instance variable

--- program_6.py
+++ program_6.py
@@ -1,4 +1,9 @@
 # Create a class
 class TV:
 # Add constructor and instance variable
+    def __init__(self, tv_name, channel, volume_level, on):
+        self.tvName = tv_name
+        self.channel = channel
+        self.volumeLevel = volume_level
+        self.on = on
 # Add instance methods
